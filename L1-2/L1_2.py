12b0fa2b06017ee81f84db25a16c0b8c03d032e2
Rodrigo José <65702187+rodrigojbsantos@users.noreply.github.com>
2021-06-03 18:30:59 -0300

    Create L1_2.py

---
+++ L1-2/L1_2.py
@@ -0,0 +1,5 @@
+# Aluno: Rodrigo Jose B dos Santos
+# rjbs2008@gmail.com
+lado1, lado2 = input().split() # leio as duas entradas
+# uso o metodo split (para tipo string)
+print(int(lado1)*int(lado2)) # mudo o tipo para int e calculo a area
