834cf2f767267ca3e1f7b57a74984af086a96990
Rodrigo José <65702187+rodrigojbsantos@users.noreply.github.com>
2021-06-03 18:22:39 -0300

    Create L1_1.py

---
+++ L1-1/L1_1.py
@@ -0,0 +1,5 @@
+# Aluno: Rodrigo Jose B dos Santos
+# rjbs2008@gmail.com
+numero = int(input()) # aqui leio o numero tipado como int
+numero += 1 # somo 1 ao proprio numero
+print(numero) # imprimo o resultado
